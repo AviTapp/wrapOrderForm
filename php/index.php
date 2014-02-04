8bd38a2b11cadef8e41da6ddedc19ecc0d37e390
Avi Tapp <andrew.tapp@gmail.com>
2014-02-04 17:37:28 +0000

    update

--- php/index.php
+++ php/index.php
@@ -7,8 +7,13 @@
     <div class="content">
       <h1>Thursday Wraps/8th grade fundraiser</h1>
       <form>
-        Name on Order: <input type="text" name="orderName" />
-
+        Name on Order: <br>
+        <input type="text" name="orderName"><br>
+        Which wrap would you like?<br>
+        <input type="radio" name="orderType" value="Garden Vegetable Wrap">Garden Vegetable Wrap<br>
+        <input type="radio" name="orderType" value="California Veggie Wrap">California Veggie Wrap<br>
+        <input type="radio" name="orderType" value="Tuna Wrap">Tuna Wrap<br>
+        
       </form>
     </div>
   </body>
